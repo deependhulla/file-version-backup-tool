a2dcdb3d2245563a4352c4697fada075b409b64d
Deepen Dhulla <deepen@deependhulla.com>
2020-01-23 18:36:46 +0530

    update 2020-01-23

--- fvb-take-backup.php
+++ fvb-take-backup.php
@@ -8,7 +8,7 @@ $dobk=0;
 $source_folder=$argv[1];
 $des_folder=$argv[2];
 $backup_title=$argv[3];
-$pass_file=$argv[4];
+
 
 if($source_folder !="" && $des_folder!="" && $backup_title!="")
 {
@@ -26,14 +26,14 @@ $cdate=date('Y-m-d_H-i-s');
 $inprogess_folder="";
 $des=$des_folder."/".$backup_title."_".$cdate;
 $latest_link=$des_folder."/".$backup_title."_latest_link";
+$rep=$des_folder."/".$backup_title."_".$cdate."_report.txt";
 $cmdx="rsync -av  \"".$source_folder."\" \"".$des."/\" --link-dest=\"".$latest_link."\"";
-
 print $cmdx."\n";
-
-
 $cmdx="rm \"".$latest_link."\" ;  ln -vs \"".$des."/\" \"".$latest_link."\"";
 print $cmdx."\n";
-
+$cmdx="find \"".$des."/\" -type f -links 1 -exec du -sh  {} \; > \"".$rep."\"";
+print $cmdx."\n";
+print "echo \"Read file for  ".$rep." \"  ";
 print "\n";
 }
 
--- temp-backup.sh
+++ temp-backup.sh
@@ -1,3 +1,4 @@
-rsync -av  "/home/mailadmin/Documents" "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-24-01/" --link-dest="/home/mailadmin/timebackup/dvddocs_latest_link"
-rm "/home/mailadmin/timebackup/dvddocs_latest_link" ;  ln -vs "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-24-01/" "/home/mailadmin/timebackup/dvddocs_latest_link"
-
+rsync -av  "/home/mailadmin/Documents" "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-32-13/" --link-dest="/home/mailadmin/timebackup/dvddocs_latest_link"
+rm "/home/mailadmin/timebackup/dvddocs_latest_link" ;  ln -vs "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-32-13/" "/home/mailadmin/timebackup/dvddocs_latest_link"
+find "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-32-13/" -type f -links 1 -exec du -sh  {} \; > "/home/mailadmin/timebackup/dvddocs_2020-01-23_18-32-13_report.txt"
+echo "Read file for  /home/mailadmin/timebackup/dvddocs_2020-01-23_18-32-13_report.txt "  
