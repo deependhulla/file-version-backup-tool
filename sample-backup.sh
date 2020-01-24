fb9dbf9b414f560db74cdc0075b393cd19c67536
Deepen Dhulla <deepen@deependhulla.com>
2020-01-24 16:16:49 +0530

    update

--- sample-backup.sh
+++ sample-backup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+##Syntax for backup : 
+##php fvb-take-backup.php <SOURCE_FOLDER> <DEST_FOLDER> <BACKUP_TITLE> 
+##Example : 
+
 php fvb-take-backup.php "/home/mailadmin/Documents" "/home/mailadmin/timebackup" "dvddocs" > temp-backup.sh
 sh temp-backup.sh
 
